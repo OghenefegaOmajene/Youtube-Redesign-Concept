ea206e3347a4bfac4609eb83cddcc796826e2306
Oghenefega Omajene <omajeneoghenefega11@gmail.com>
2025-05-01 15:59:41 +0100

    Sidenav design

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
 import './App.css';
 import AppRouter from './AppRouter';
-import Sidebar from './components/Sidebar/Sidebar';
 
 function App() {
 
   return (
     <>
-      <div className="App">
-        <Sidebar/>
         <AppRouter/>
-      </div>
     </>
   )
 }
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,29 +1,40 @@
 import React from 'react'
 import './Sidebar.css'
 import { Link } from 'react-router-dom'
-import Logo from '../../components/img/Logo.png'
+import Logo from '../../assets/Logo.png'
+import { IoMenu } from "react-icons/io5";
+import { SiYoutubeshorts } from "react-icons/si";
+import { IoMdHome } from "react-icons/io";
+import { MdOutlineExplore } from "react-icons/md";
+import { MdHistory } from "react-icons/md";
+import { RxClock } from "react-icons/rx";
+import { FiThumbsUp } from "react-icons/fi";
+import { CgPlayList } from "react-icons/cg";
+import { MdOutlineSubscriptions } from "react-icons/md";
+import { FiGrid } from "react-icons/fi";
 
 const Sidebar = () => {
   return (
     <div className='sideBar'>
         <div className='logo'>
-            {Logo}
+            <i><IoMenu /></i>
+            <img src={Logo} alt="" />
         </div>
         <div className='box1'>
-            <Link to="/Home" >Home</Link>
-            <Link to="" >Explore</Link>
-            <Link to="/Shorts" >Shorts</Link>
+            <Link to="/Home" ><i><IoMdHome /></i> Home</Link>
+            <Link to="" ><i><MdOutlineExplore /></i>Explore</Link>
+            <Link to="/Shorts" ><i><SiYoutubeshorts /></i> Shorts</Link>
             <Link to="" >TV Mode</Link>
         </div>
         <div className='box2'>
-            <Link to="" >History</Link>
-            <Link to="" >Watch Later</Link>
-            <Link to="" >Liked Videos</Link>
-            <Link to="" >Playlists</Link>
+            <Link to="" > <i><MdHistory /></i>History</Link>
+            <Link to="" > <i><RxClock /></i>Watch Later</Link>
+            <Link to="" > <i><FiThumbsUp /></i>Liked Videos</Link>
+            <Link to="" ><i><CgPlayList /></i>Playlists</Link>
         </div>
         <div className='box3'>
-            <Link to="" >Collections</Link>
-            <Link to="" >Subscriptions</Link>
+            <Link to="" ><i><FiGrid /></i>Collections</Link>
+            <Link to="" > <i><MdOutlineSubscriptions /></i>Subscriptions</Link>
         </div>
     </div>
   )
